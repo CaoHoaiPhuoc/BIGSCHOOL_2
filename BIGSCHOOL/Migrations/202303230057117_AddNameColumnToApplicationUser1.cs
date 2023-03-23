303c25a1ae561e8277bcfd353ae90eb5307c2714
Administrator <Administrator@21AK22-COM>
2023-03-23 09:39:28 +0700

    Add project files.

---
+++ BIGSCHOOL/Migrations/202303230057117_AddNameColumnToApplicationUser1.cs
@@ -0,0 +1,20 @@
+﻿namespace BIGSCHOOL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToApplicationUser1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "DateTime", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Courses", "Datatime");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "Datatime", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Courses", "DateTime");
+        }
+    }
+}
